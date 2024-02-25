1c9fcbba164cfcd26438a45e064543e6c8d56066
RobF <raam337@gmail.com>
2024-02-25 14:02:40 +0000

    Person objects complete

--- index.js
+++ index.js
@@ -13,3 +13,10 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+inquirer.createPromptModule([
+    {type:"list",
+    name:"action",
+    message:"Select action:",
+    choices:["Add an engineer","Add an intern","Finish building the team"]
+    },
+])
